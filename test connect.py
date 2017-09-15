62da3c501f5e51387305bba1ab707a751a43123d
rafiramadhan <rafi.ramadhan27@yahoo.com>
2017-09-16 01:46:22 +0700

    test

---
+++ test connect.py
@@ -0,0 +1,21 @@
+import sys
+import socket
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the port
+server_address = ('localhost', 12000)
+print >>sys.stderr, 'starting up on %s port %s' % server_address
+sock.bind(server_address)
+
+# Listen for incoming connections
+sock.listen(1)
+
+while True:
+	# Wait for a connection
+	print >>sys.stderr, 'waiting for a connection'
+	socket_si_client, client_address = sock.accept()
+	print >> sys.stderr, 'connection from', client_address
+	socket_si_client.sendall('Test')
+	socket_si_client.close
